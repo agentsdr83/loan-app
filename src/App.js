98a59ddf58b62b6607d29fdc1fc979a0ebeb5af6
Yashwanth <agentsdr83@gmail.com>
2026-05-05 16:39:52 +0530

    ui redirection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // App.js
 
 import { useAuth } from "react-oidc-context";
+import LoanForm from "./LoanForm";
 
 function App() {
   const auth = useAuth();
@@ -22,14 +23,7 @@ function App() {
 
   if (auth.isAuthenticated) {
     return (
-      <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-        <button onClick={() => auth.removeUser()}>Sign out</button>
-      </div>
+      <LoanForm />
     );
   }
 
